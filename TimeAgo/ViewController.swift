df0e128adc98c09a1062d7d8082ede7e82fce13b
Parth <parth0072@live.com>
2020-07-20 15:02:07 +0530

    Initial Commit

---
+++ TimeAgo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeAgo
+//
+//  Created by Parth on 20/07/20.
+//  Copyright © 2020 Parth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
